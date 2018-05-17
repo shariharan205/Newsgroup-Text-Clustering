41f228dabe9089013308f56eb60f6982d89cc80f
shariharan205 <shariharan205@gmail.com>
2018-05-17 16:01:39 -0700

    Creating a run file

---
+++ run.py
@@ -0,0 +1,12 @@
+from Clustering import Clustering
+
+def breakpoint():
+    """
+
+    Allows the flow of program based on the control of the user.
+    """
+    print "\nContinue? [y/n] : "
+    inp = raw_input()
+
+    if inp != 'y':
+        exit()
